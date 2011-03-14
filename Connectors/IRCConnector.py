ccbc992de4d2ba37bed68834af3e98a260186e8b
Steven <theepicsnail@gmail.com>
2011-03-13 23:28:24 -0400

    Added Logging and a 'stop' feature

--- Connectors/IRCConnector.py
+++ Connectors/IRCConnector.py
@@ -1,10 +1,12 @@
 from Interfaces import Connector
 from Configuration import ConfigFile
+from Logging import LogFile
 from zope.interface import implements
 from twisted.words.protocols import irc
 from twisted.internet import protocol,reactor
 import sys,traceback
 #these events get given to plugins to give back to the the ircconnector
+log = LogFile("IRCConnector")
 class IRCConnectorEvents:
 
     def __init__(self): pass
@@ -37,23 +39,33 @@ class IRCConnectorEvents:
         return "action",channel,message
     def ping(self,user,message=""):
         return "ping",user,message
-
+    def stop(self):
+        return "stop"
 
 class IRCConnector(protocol.ClientFactory,irc.IRCClient,object):
     implements(Connector)
     EventHandler = None
-
+    reactor = None
     def HandleResponse(self,eventInfo):        
+        if eventInfo=="stop":
+            log.debug("Stopping reactor")
+            self.reactor.stop()
+            self.transport.doWrite()
+            self.reactor.stop()
+            return 
         f = getattr(self,eventInfo[0],None)
         if f:
             try:
-                print "Calling:",f,eventInfo[1:]
+                log.debug("Calling",f,eventInfo[1:])
                 f(*eventInfo[1:])
-                self.transport.doWrite()
+                self.transport.doWrite() #Coming from a different thread, we have to do this apparently
             except:
                 traceback.print_exc(file=sys.stdout)
+
     def SetEventHandler(self,func):
+        log.debug("Event handler set to",func)
         self.EventHandler=func
+
     def buildProtocol(self,addr):
         return self
 
@@ -67,40 +79,41 @@ class IRCConnector(protocol.ClientFactory,irc.IRCClient,object):
             event["target"]=params[0]
             if len(params)==2:
                 event["message"]=params[1]
-            print "Connector is producing ->"
-            print params
-            print event
+            log.debug("Producing Event",event)
             self.EventHandler(event) 
 
 
     def __init__(self):
+        log.note("Constructing.")
         self.eventObj  = IRCConnectorEvents()
         self.protocol = self
         self.config = ConfigFile("IRCConnector")
-
+        
     def Start(self): 
-        print "Connect", self.config
         
         server = self.config["Connection","Server"]
         port = int(self.config["Connection","Port"])
         nick = self.config["Connection","Nick"]
 
+        log.note("Connecting",server,port,nick)
         if not server:
-            print "No 'Connection:Server' specified."
+            log.fatal("No 'Connection:Server' specified.")
             return
         if not port: 
-            print "No 'Connection:Port' specified."
+            log.fatal("No 'Connection:Port' specified.")
             return
         if not nick: 
-            print "No 'Connection:Nick' specified."
+            log.fatal("No 'Connection:Nick' specified.")
             return
 
         self.nickname=nick
         reactor.connectTCP(server,port,self)
+        log.note("Running reactor")
+        self.reactor = reactor
         reactor.run()
 
     def Stop(self): 
-        print "Disconnect"
+        log.note("Nothing to do for stopping")
 
     def GetResponseObject(self):
         return self.eventObj
--- Plugins/Test.py
+++ Plugins/Test.py
@@ -3,6 +3,8 @@ class Test:
     @bindFunction(command="PRIVMSG",prefix="(?P<nick>.*)!.*")
     def foo(self,response,nick,message):
         print "== foo"
+        if message=="stop":
+            return response.stop()
         return response.msg(nick,message)
         
     @bindFunction(command="INVITE")
